b8ad0b8e5126c01609a46b20151e8f5b7c3f1539
Bamboogamer <bamboogamer178@gmail.com>
2023-03-10 20:31:16 -0600

    New Features
    - Added anonymized loan data
    - Rollover pay when payment is more then the current principle
    - Added logging for when loans are paid off

--- StudentLoanCalculator.py
+++ StudentLoanCalculator.py
@@ -1,65 +1,97 @@
+def studentLoans(loans, interestRates, amountPaid):
+    numOfLoans = len(loans)
+    month = 0
 
-loans = \
-    {
-    }
+    while loans[1] > 0:
+        month += 1
+        totalInterest = calculateInterest(loans, interestRates)
+        highestLoan = findFirstHighestRate(loans, interestRates)
 
-interestRates = \
-    {
-    }
+        payLoan(loans, interestRates, highestLoan, amountPaid, totalInterest)
+        printLoanInfo(loans, amountPaid, totalInterest, month, numOfLoans)
+
+    print(
+          f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+          f"* ALL LOANS HAVE BEEN PAID OFF in ~{month} Months! *\n"
+          f"********** Approximately {month / 12:.2f} years! **********\n"
+          f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+    )
 
 
-def studentLoans(loans, interestRates, principleCost, years):
+def calculateInterest(loans, interestRates):
+    totalInterest = 0
+    for key in list(loans.keys()):
+        interest_cost = (interestRates[key] / 12) * loans[key]
+        loans[key] += interest_cost
+        totalInterest += interest_cost
+    return totalInterest
 
-    for i in range(years*12):
-        interest_cost1 = interestRates[1]*loans[1]
-        interest_cost2 = interestRates[2]*loans[2]
-        loans[1] += interest_cost1
-        loans[2] += interest_cost2
 
-        interest_cost3 = interestRates[3]*loans[3]
-        interest_cost4 = interestRates[4]*loans[4]
-        loans[3] += interest_cost3
-        loans[4] += interest_cost4
+def findFirstHighestRate(loans, interestRates):
+    keys = list(interestRates.keys())
+    highestRateLoanKey = len(interestRates.keys())
+    for key in keys:
+        if interestRates[key] >= highestRateLoanKey and loans[key] > 0:
+            highestRateLoanKey = key
+    return highestRateLoanKey
 
-        interest_cost5 = interestRates[5]*loans[5]
-        interest_cost6 = interestRates[6]*loans[6]
-        loans[5] += interest_cost5
-        loans[6] += interest_cost6
 
-        # Assuming you're paying of the interest for EVERY Loan FIRST
-        loans[1] -= interest_cost1
-        loans[2] -= interest_cost2
-        loans[3] -= interest_cost3
-        loans[4] -= interest_cost4
-        loans[5] -= interest_cost5
-        loans[6] -= interest_cost6
+def payLoan(loans, interestRates, highestLoan, amountPaid, totalInterest):
+    loans[highestLoan] -= amountPaid - totalInterest
 
-        # Paying off the Principle
+    if highestLoan != 1 and loans[highestLoan] <= 0:
+        loans[highestLoan - 1] += loans[highestLoan]
+        print(
+              f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+              f"LOAN {highestLoan} PAID OFF! :D\n"
+              f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+        del loans[highestLoan]
+        del interestRates[highestLoan]
 
-        # Find the highest Interest Rate
-        # USE HIGHEST_Interest, find the highest loan WITH the highest interest rate
-        # remove WHOLE principle towards that loan until it is less than or equal to zero
 
-        # If it's less than 0, convert the negative balance and add it to the NEXT payment of the next largest loan
-        # Then REMOVE that loan from the dictionary
+def printLoanInfo(loans, amountPaid, totalInterest, month, totalNumOfLoans):
 
-        # Hardcoded
-        loans[6] -= principleCost
+    print(f"MONTH {month}\n"
+          f"Total Amount Paid this Month: ${amountPaid:.2f}\n"
+          f"Principle Paid this Month: ${amountPaid - totalInterest:.2f}\n"
+          f"Total Interest Paid this Month: ${totalInterest:.2f}"
+          )
 
-        # Can be helper function
-        totalInterest = interest_cost1 + interest_cost2 + \
-                        interest_cost3 + interest_cost4 + \
-                        interest_cost5 + interest_cost6
+    print("Current Loans:\n"
+          "===============================")
 
-        print(f"MONTH {i+1}\n"
-              f"Total Interest {totalInterest}\n")
-        print(f"Loan Balances:\n"
-              f"Loan 1: {loans[1]:.2f}\n"
-              f"Loan 2: {loans[2]:.2f}\n"
-              f"Loan 3: {loans[3]:.2f}\n"
-              f"Loan 4: {loans[4]:.2f}\n"
-              f"Loan 5: {loans[5]:.2f}\n"
-              f"Loan 6: {loans[6]:.2f}\n"
-              )
+    for loanNumber in range(1, totalNumOfLoans+1):
 
-studentLoans(loans, interestRates, 500, 2)
+        try:
+            if loans[1] <= 0:
+                print(f"Loan {loanNumber} Balance: [PAID OFF]")
+            else:
+                print(f"Loan {loanNumber} Balance: ${loans[loanNumber]:.2f}")
+        except KeyError:
+            print(f"Loan {loanNumber} Balance: [PAID OFF]")
+
+    print(f"END OF MONTH {month + 1}\n")
+
+
+if __name__ == '__main__':
+    myLoans = \
+        {
+            1: 10000,
+            2: 10000,
+            3: 10000,
+            4: 10000,
+            5: 10000,
+            6: 10000
+        }
+
+    myInterestRates = \
+        {
+            1: 0.0275,
+            2: 0.0275,
+            3: 0.0373,
+            4: 0.0373,
+            5: 0.0499,
+            6: 0.0499
+        }
+
+    studentLoans(myLoans, myInterestRates, 750)
