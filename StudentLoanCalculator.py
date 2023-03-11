161c1158b47ab54792fef36e0c766407dc2590ac
Bamboogamer <bamboogamer178@gmail.com>
2023-03-10 21:48:33 -0600

    Update StudentLoanCalculator.py
    - Added stopping points to calculator
      - Loan "AmountPaid" must be enough to play within 16 years
      - Amount paid per month MUST be above the interest accrued per month
    - Added a method to find MINIMUM amount needed to pay off loan in X amount of years

--- StudentLoanCalculator.py
+++ StudentLoanCalculator.py
@@ -1,11 +1,24 @@
+import copy
+
+
 def studentLoans(loans, interestRates, amountPaid):
     numOfLoans = len(loans)
     month = 0
 
     while loans[1] > 0:
         month += 1
+
+        # If payment amount will not be complete within 20 years
+        if month > (16*12):
+            print("LOAN CANNOT BE PAID OFF WITHIN 16 YEARS, RECONSIDER HOW MUCH YOU CAN PAY PER MONTH")
+            return -1
+
         totalInterest = calculateInterest(loans, interestRates)
         highestLoan = findHighestInterestRateWithOutstandingPrinciple(loans, interestRates)
+
+        if (totalInterest > amountPaid):
+            print(f"Amount {amountPaid} Paid Per Month will not cover interest")
+            return -1
         payLoan(loans, interestRates, highestLoan, amountPaid, totalInterest)
         printLoanInfo(loans, amountPaid, totalInterest, month, numOfLoans)
 
@@ -15,6 +28,7 @@ def studentLoans(loans, interestRates, amountPaid):
           f"********** Approximately {month / 12:.2f} years! **********\n"
           f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
     )
+    return month
 
 
 def calculateInterest(loans, interestRates):
@@ -77,11 +91,29 @@ def printLoanInfo(loans, amountPaid, totalInterest, month, totalNumOfLoans):
         except KeyError:
             print(f"Loan {loanNumber} Balance: [PAID OFF]")
 
-    print(f"END OF MONTH {month + 1}\n")
+    print(f"END OF MONTH {month}\n")
+
+
+def minimumPaymentPerMonth(loans, interestRates, goalYears):
+    goalMonths = goalYears * 12
+
+    # Starting at a bare minimum of 100 dollars
+    amount = 100
+    while True:
+        amount += 1
+        testMonths = studentLoans(copy.deepcopy(loans), copy.deepcopy(interestRates), amount)
+
+        if goalMonths >= testMonths > 0:
+            break
+
+    print(f"TESTING DANNY: MINIUM AMOUNT REQUIRED --> {amount}")
+    return amount
+
+
 
 
 if __name__ == '__main__':
-    myLoans = \
+    myNormalLoans = \
         {
             1: 10000,
             2: 10000,
@@ -105,6 +137,7 @@ if __name__ == '__main__':
             6: 0.0499
         }
 
-    studentLoans(myNormalLoans, myInterestRates, 750)
-
-    # studentLoans(myForgivenLoans, myInterestRates, 750)
+    # studentLoans(myNormalLoans, myInterestRates, 750)
+    #
+    # # studentLoans(myForgivenLoans, myInterestRates, 750)
+    minimumPaymentPerMonth(myNormalLoans, myInterestRates, 2)
